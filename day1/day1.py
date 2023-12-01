21b499a9061d1a8807f24c528370a3d2ef7e45d4
Sam Levine <samuellevine@cs.uchicago.edu>
2023-12-01 03:21:54 -0600

    finished part 2 aka finished day 1

--- day1/day1.py
+++ day1/day1.py
@@ -49,7 +49,6 @@ def get_input():
     with open('input.txt', 'r') as f:
         return f.read().splitlines()
     
-
 def string_to_value(string):
     """
     Convert a string to a value.
@@ -64,9 +63,54 @@ def string_to_value(string):
 
     return int(string[i] + string[j])
 
+"""
+--- Part Two ---
+
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+"""
+
+def word_to_value(word):
+    nums = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+
+    i = 0
+    while i < len(word):
+        for spelled, digit in nums.items():
+            # for each spelled out number, check if the word starts with it
+            if word[i:].startswith(spelled):
+                # if it does, replace it with the digit and move on
+                word = word[:i] + digit + word[i+len(spelled):]
+                i += len(digit) - 1
+                break
+        i += 1
+    return word
 
 def main():
     strings = get_input()
+    strings = [word_to_value(string) for string in strings]
     total = sum(string_to_value(string) for string in strings)
     print(total)
 
