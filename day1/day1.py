76ee6daf48e7181a9568f66de525f32f04302cc6
Sam Levine <samuellevine@cs.uchicago.edu>
2023-12-03 23:45:20 -0600

    fixed typo

--- day1/day1.py
+++ day1/day1.py
@@ -37,7 +37,7 @@ a1b2c3d4e5f
 treb7uchet
 
 In this example, the calibration values of these four lines are 
-12, 38, 15, and77. Adding these together produces 142.
+12, 38, 15, and 77. Adding these together produces 142.
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 """
