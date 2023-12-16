5bdc9e1d6b8bc1f1a6e6e4fdeeee310948fc722f
Sam Levine <samlevine03@gmail.com>
2023-12-15 21:55:37 -0500

    solved day 8 part 1, but part 2 is tricky! need a new approach

--- day8/day8.py
+++ day8/day8.py
@@ -40,19 +40,104 @@ ZZZ = (ZZZ, ZZZ)
 Starting at `AAA` , follow the left/right instructions. *How many steps are required to reach `ZZZ` ?*
 """
 
+def part1(instructions, nodes):
+    curr = "AAA"
+    instr_idx = 0
+    steps = 0
+    while curr != "ZZZ":
+        if instructions[instr_idx] == "R":
+            curr = nodes[curr][1]
+        elif instructions[instr_idx] == "L":
+            curr = nodes[curr][0]
+        instr_idx = (instr_idx + 1) % len(instructions)
+        steps += 1
+    return steps
+        
 
 
 """
-Day 8 Part 2 Instructions Go Here
+--- Part Two ---
+
+The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the instructions, but you've barely left your starting position. It's going to take significantly more steps to escape!
+
+What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the laws of spacetime? Only one way to find out.
+
+After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z.
+
+For example:
+
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+
+Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+    Step 0: You are at 11A and 22A.
+    Step 1: You choose all of the left paths, leading you to 11B and 22B.
+    Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+    Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+    Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+    Step 5: You choose all of the left paths, leading you to 11B and 22C.
+    Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+
+So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+
 """
 
 
+def part2(instructions, nodes_list):
+    nodes_dict = {node.split(" = ")[0]: node.split(" = ")[1].strip("()").split(", ") for node in nodes_list}
+    curr_nodes = curr_nodes = [node for node in nodes_dict if node.endswith("A")]
+
+    steps = 0
+    instr_idx = 0
+    while not all(node.endswith("Z") for node in curr_nodes):
+        # print how many nodes ends with Z
+        num_z = sum(node.endswith("Z") for node in curr_nodes)
+        if num_z > 2:
+            print(curr_nodes, num_z, steps)
 
 
+        direction = instructions[instr_idx]
+        curr_nodes = [nodes_dict[node][0 if direction == "L" else 1] for node in curr_nodes]
+        instr_idx = (instr_idx + 1) % len(instructions)
+        steps += 1
+
+    print(curr_nodes)
+
+    return steps
 
 # -------- MAIN FUNCTION -------- #
 def main():
     lines = get_input()
+    instructions = lines[0]
+    nodes = lines[2:]
+
+    # for part 2, note all of the nodes that end with A and Z
+    A_idxs, Z_idxs = [], []
+    for idx, node in enumerate(nodes):
+        if node.split(" = ")[0].endswith("A"):
+            A_idxs.append(idx)
+        elif node.split(" = ")[0].endswith("Z"):
+            Z_idxs.append(idx)
+    print(A_idxs, Z_idxs)
+
+    nodes_dict = {node.split(" = ")[0]: node.split(" = ")[1].replace("(", "").replace(")", "").split(", ") for node in nodes}
+    num_steps = part1(instructions, nodes_dict)
+    print(f"Part 1: {num_steps}")
+
+    num_steps = part2(instructions, nodes)
+    print(f"Part 2: {num_steps}")
+
     pass
 
 if __name__ == "__main__":
