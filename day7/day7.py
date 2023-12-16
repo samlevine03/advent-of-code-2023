37459cf3f2a16bafb4dcc23a586fc4f3dbc04ee2
Sam Levine <samlevine03@gmail.com>
2023-12-15 21:08:00 -0500

    finished day 7

--- day7/day7.py
+++ day7/day7.py
@@ -60,20 +60,157 @@ Now, you can determine the total winnings of this set of hands by adding up the 
 Find the rank of every hand in your set. *What are the total winnings?*
 """
 
+def get_type(hand):
+    """
+    Returns the rank of a hand
+    """
+    type = 0
+    counts = {card: hand.count(card) for card in hand}
+    if 5 in counts.values():
+        type = 7
+    elif 4 in counts.values():
+        type = 6
+    elif 3 in counts.values() and 2 in counts.values():
+        type = 5
+    elif 3 in counts.values():
+        type = 4
+    elif list(counts.values()).count(2) == 2:
+        type = 3
+    elif 2 in counts.values():
+        type = 2
+    else:
+        type = 1
+    return type
+
+
+def hand_to_sort_value(hand):
+    """
+    Convert a hand to a tuple representing its sort order.
+    """
+    card_values = {"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
+    return tuple(card_values[card] for card in hand)
 
 
 """
-Day 7 Part 2 Instructions Go Here
+--- Part Two ---
+
+To make things a little more interesting, the Elf introduces one additional rule. Now, J cards are jokers - wildcards that can act like whatever card would make the hand the strongest type possible.
+
+To balance this, J cards are now the weakest individual cards, weaker even than 2. The other cards stay in the same order: A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J.
+
+J cards can pretend to be whatever card is best for the purpose of determining hand type; for example, QJJQ2 is now considered four of a kind. However, for the purpose of breaking ties between two hands of the same type, J is always treated as J, not the card it's pretending to be: JKKK2 is weaker than QQQQ2 because J is weaker than Q.
+
+Now, the above example goes very differently:
+
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+
+    32T3K is still the only one pair; it doesn't contain any jokers, so its strength doesn't increase.
+    KK677 is now the only two pair, making it the second-weakest hand.
+    T55J5, KTJJT, and QQQJA are now all four of a kind! T55J5 gets rank 3, QQQJA gets rank 4, and KTJJT gets rank 5.
+
+With the new joker rule, the total winnings in this example are 5905.
+
+Using the new joker rule, find the rank of every hand in your set. What are the new total winnings?
+
 """
 
+def get_type_joker(hand):
+    """
+    Returns the rank of a hand with jokers
+    """
+    old_type = get_type(hand)
+    num_jokers = hand.count("J")
+    hand_without_jokers = hand.replace("J", "")
+    without_jokers_type = get_type(hand_without_jokers)
+
+    # handle case with 4 or 5 jokers
+    if num_jokers >= 4:
+        return 7
+    
+    # handle case with 3 jokers
+    if num_jokers == 3:
+        # we can either get 4 or 5 of a kind
+        return 6 if without_jokers_type == 1 else 7
+
+    # handle case with 2 jokers
+    if num_jokers == 2:
+        # we can either get 3, 4, or 5 of a kind
+        if without_jokers_type == 1:
+            return 4
+        elif without_jokers_type == 2:
+            return 6
+        return 7
+    
+    # handle case with 1 joker
+    if num_jokers == 1:
+        # we can get 2, 3, 4, or 5 of a kind
+        if without_jokers_type == 1:
+            return 2
+        elif without_jokers_type == 2:
+            return 4
+        elif without_jokers_type == 3:
+            return 5
+        elif without_jokers_type == 4:
+            return 6
+        return 7
+    
+    # handle case with no jokers
+    return old_type
 
 
+def hand_to_sort_value_joker(hand):
+    """
+    Convert a hand to a tuple representing its sort order with jokers.
+    """
+    card_values = {"A": 14, "K": 13, "Q": 12, "J": 0, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
+    return tuple(card_values[card] for card in hand)
 
 
 # -------- MAIN FUNCTION -------- #
 def main():
     lines = get_input()
-    pass
+
+    # hands_by_type = {
+    #     1: [],  # High card
+    #     2: [],  # One pair
+    #     3: [],  # Two pair
+    #     4: [],  # Three of a kind
+    #     5: [],  # Full house
+    #     6: [],  # Four of a kind
+    #     7: []   # Five of a kind
+    # }
+    # for line in lines:
+    #     hand, bid = line.split()
+    #     hand_type = get_type(hand)
+    #     hands_by_type[hand_type].append((hand, bid))
+
+    hands = [(get_type(hand), hand, int(bid)) for line in lines for hand, bid in [line.split()]]
+
+    # for type in hands_by_type:
+    #     hands_by_type[type].sort(key=lambda x: hand_to_sort_value(x[0]))
+
+    sorted_hands = sorted(hands, key=lambda x: (x[0], hand_to_sort_value(x[1])), reverse=True)
+
+    total_score = 0
+    # rank = 1
+    # for type in hands_by_type:
+    #     for hand, bid in hands_by_type[type]:
+    #         total_score += rank * int(bid)
+    #         rank += 1
+
+    total_score = sum((len(hands) - rank + 1) * bid for rank, (_, _, bid) in enumerate(sorted_hands, start=1))
+
+    print(f"Part 1 Score: {total_score}")
+
+    # Part 2
+    hands = [(get_type_joker(hand), hand, int(bid)) for line in lines for hand, bid in [line.split()]]
+    sorted_hands = sorted(hands, key=lambda x: (x[0], hand_to_sort_value_joker(x[1])), reverse=True)
+    total_score = sum((len(hands) - rank + 1) * bid for rank, (_, _, bid) in enumerate(sorted_hands, start=1))
+    print(f"Part 2 Score: {total_score}")
 
 if __name__ == "__main__":
     main()
