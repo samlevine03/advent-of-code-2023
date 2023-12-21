95513a0165490a8bd3363a0afe67fdd0ca7eb390
Sam Levine <samlevine03@gmail.com>
2023-12-21 02:56:27 -0500

    more pythonic

--- day08/day08.py
+++ day08/day08.py
@@ -1,18 +1,15 @@
 # https://adventofcode.com/2023/day/08
 
 from utils import get_input
+from math import gcd
+from functools import reduce
 
 # Part 1
 
 def part1(instructions, nodes):
-    curr = "AAA"
-    instr_idx = 0
-    steps = 0
+    curr, instr_idx, steps = "AAA", 0, 0
     while curr != "ZZZ":
-        if instructions[instr_idx] == "R":
-            curr = nodes[curr][1]
-        elif instructions[instr_idx] == "L":
-            curr = nodes[curr][0]
+        curr = nodes[curr][0 if instructions[instr_idx] == "L" else 1]
         instr_idx = (instr_idx + 1) % len(instructions)
         steps += 1
     return steps
@@ -20,56 +17,26 @@ def part1(instructions, nodes):
 # Part 2
 
 def part2(instructions, nodes_dict):
-    curr_nodes = curr_nodes = [node for node in nodes_dict if node.endswith("A")]
+    curr_nodes = [node for node in nodes_dict if node.endswith("A")]
 
     def finish_node(start):
-        curr = start
-        instr_idx = 0
-        steps = 0
+        curr, instr_idx, steps = start, 0, 0
         while not curr.endswith("Z"):
-            if instructions[instr_idx] == "R":
-                curr = nodes_dict[curr][1]
-            elif instructions[instr_idx] == "L":
-                curr = nodes_dict[curr][0]
+            curr = nodes_dict[curr][0 if instructions[instr_idx] == "L" else 1]
             instr_idx = (instr_idx + 1) % len(instructions)
             steps += 1
         return steps
     
     steps_for_each = [finish_node(node) for node in curr_nodes]
-
-    # find the LCM of all of the steps
-    def gcd(a, b):
-        if a == 0:
-            return b
-        return gcd(b % a, a)
-    
-    def lcm(a, b):
-        return (a * b) // gcd(a, b)
-    
-    def find_lcm(steps):
-        lcm_val = steps[0]
-        for i in range(1, len(steps)):
-            lcm_val = lcm(lcm_val, steps[i])
-        return lcm_val
-    
-    return find_lcm(steps_for_each)
+    return reduce(lambda a, b: (a * b) // gcd(a, b), steps_for_each)
 
 # -------- MAIN FUNCTION -------- #
 def main():
     lines = get_input()
     instructions = lines[0]
     nodes = lines[2:]
-
-    # for part 2, note all of the nodes that end with A and Z
-    A_idxs, Z_idxs = [], []
-    for idx, node in enumerate(nodes):
-        if node.split(" = ")[0].endswith("A"):
-            A_idxs.append(idx)
-        elif node.split(" = ")[0].endswith("Z"):
-            Z_idxs.append(idx)
-    print(A_idxs, Z_idxs)
-
     nodes_dict = {node.split(" = ")[0]: node.split(" = ")[1].replace("(", "").replace(")", "").split(", ") for node in nodes}
+    
     num_steps = part1(instructions, nodes_dict)
     print(f"Part 1: {num_steps}")
 
