1d44c16183f270d65cc10390209390a59c25f378
Sam Levine <samlevine03@gmail.com>
2023-12-21 02:17:05 -0500

    day10 solved

--- day10/day10.py
+++ day10/day10.py
@@ -158,7 +158,6 @@ def traverse(arr):
             if j + dir[1] < 0 or j + dir[1] >= len(arr[i]):
                 continue
             pipe = arr[i + dir[0]][j + dir[1]]
-            # print(dir, pipe)
             if dir == (0, -1) and pipe in 'L-F':
                 return dir
             if dir == (0, 1) and pipe in 'J-7':
@@ -214,8 +213,222 @@ def traverse(arr):
 
 
 """
-Day 10 Part 2 Instructions Go Here
+--- Part Two ---
+
+You quickly reach the farthest point of the loop, but the animal never emerges. Maybe its nest is within the area enclosed by the loop?
+
+To determine whether it's even worth taking the time to search for such a nest, you should calculate how many tiles are contained within the loop. For example:
+
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+
+The above loop encloses merely four tiles - the two pairs of . in the southwest and southeast (marked I below). The middle . tiles (marked O below) are not in the loop. Here is the same loop again with those regions marked:
+
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+
+In fact, there doesn't even need to be a full tile path to the outside for tiles to count as outside the loop - squeezing between pipes is also allowed! Here, I is still within the loop and O is still outside the loop:
+
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+
+In both of the above examples, 4 tiles are enclosed by the loop.
+
+Here's a larger example:
+
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+
+The above sketch has many random bits of ground, some of which are in the loop (I) and some of which are outside it (O):
+
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+
+In this larger example, 8 tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the loop. Here's another example with many bits of junk pipe lying around that aren't connected to the main loop at all:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
+Here are just the tiles that are enclosed by the loop marked with I:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
+In this last example, 10 tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the area within the loop. How many tiles are enclosed by the loop?
 """
+
+def traverse_2(arr):
+    """
+    Traverse the array and return the number of steps it takes to get from the starting position to the point farthest from the starting position.
+    """
+
+    dirs = (
+        (0, -1),
+        (0, 1),
+        (-1, 0),
+        (1, 0)
+    )
+
+    def find_start(arr):
+        """
+        Find the starting position of the animal.
+        """
+        for i in range(len(arr)):
+            for j in range(len(arr[i])):
+                if arr[i][j] == 'S':
+                    return (i, j)
+
+
+    def get_starting_dir(arr, i, j):
+        """
+        Get the starting direction of the animal.
+        """
+        for d in range(len(dirs)):
+            dir = dirs[d]
+            if i + dir[0] < 0 or i + dir[0] >= len(arr):
+                continue
+            if j + dir[1] < 0 or j + dir[1] >= len(arr[i]):
+                continue
+            pipe = arr[i + dir[0]][j + dir[1]]
+            if dir == (0, -1) and pipe in 'L-F':
+                return dir
+            if dir == (0, 1) and pipe in 'J-7':
+                return dir
+            if dir == (-1, 0) and pipe in '7|F':
+                return dir
+            if dir == (1, 0) and pipe in 'J|L':
+                return dir
+            
+        return None
+
+
+    start = find_start(arr)
+    i, j = start
+
+    prev_dir = None
+    next_dir = get_starting_dir(arr, i, j)
+
+    loop = ['S']
+    loop_coords = set()
+    loop_coords.add((i, j))
+    i += next_dir[0]
+    j += next_dir[1]
+
+    while arr[i][j] != 'S':
+        pipe = arr[i][j]
+        loop.append(pipe)
+        loop_coords.add((i, j))
+        prev_dir = next_dir
+        if pipe == '|' or pipe == '-':
+            next_dir = prev_dir
+        elif pipe == 'L':
+            if prev_dir == (0, -1):
+                next_dir = (-1, 0)
+            elif prev_dir == (1, 0):
+                next_dir = (0, 1)
+        elif pipe == 'J':
+            if prev_dir == (0, 1):
+                next_dir = (-1, 0)
+            elif prev_dir == (1, 0):
+                next_dir = (0, -1)
+        elif pipe == '7':
+            if prev_dir == (0, 1):
+                next_dir = (1, 0)
+            elif prev_dir == (-1, 0):
+                next_dir = (0, -1)
+        elif pipe == 'F':
+            if prev_dir == (0, -1):
+                next_dir = (1, 0)
+            elif prev_dir == (-1, 0):
+                next_dir = (0, 1)
+        i += next_dir[0]
+        j += next_dir[1]
+
+    return loop_coords
+
+
+def count_enclosed_tiles(arr, loop_coords):
+
+    def enclosed(i, j):
+        count = 0
+        for k in range(j):
+            if not (i, k) in loop_coords:
+                continue
+            if arr[i][k] in "F7|":  # note, using JL instead of 7F didn't work since the S is a J!
+                count += 1
+        
+        if count % 2 != 0:
+            return True
+        
+        return False
+        
+    count = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if not (i, j) in loop_coords:
+                if enclosed(i, j):
+                    count += 1
+
+    return count
+
+
 
 
 # -------- MAIN FUNCTION -------- #
@@ -224,6 +437,10 @@ def main():
     arr = input_to_2d_array(lines)
     part1 = traverse(arr)
     print(f'Part 1: {part1}')
+
+    loop_coords = traverse_2(arr)
+    part2 = count_enclosed_tiles(arr, loop_coords)
+    print(f'Part 2: {part2}')
 
 
 if __name__ == "__main__":
